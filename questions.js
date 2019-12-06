9ae01fc36da1497bb93ef1b2beb3d5188ad9c91e
Patrick McDowell <kuhlthing@gmail.com>
2019-12-05 23:32:22 -0500

    moved rough drafts to repo

---
+++ questions.js
@@ -0,0 +1,83 @@
+let questions = [
+    // Question 0
+        {
+            question: ""
+            choiceA : 
+            choiceB : 
+            choiceC : 
+            correct : 
+        },
+    // Question 1
+        {
+            question: ""
+            choiceA : 
+            choiceB : 
+            choiceC : 
+            correct : 
+        },
+    // Question 2
+        {
+            question: ""
+            choiceA : 
+            choiceB : 
+            choiceC : 
+            correct : 
+        },
+    // Question 3
+        {
+            question: ""
+            choiceA : 
+            choiceB : 
+            choiceC : 
+            correct : 
+        },
+    // Question 4
+        {
+            question: ""
+            choiceA : 
+            choiceB : 
+            choiceC : 
+            correct : 
+        },
+    // Question 5
+        {
+            question: ""
+            choiceA : 
+            choiceB : 
+            choiceC : 
+            correct : 
+        },
+    // Question 6
+        {
+            question: ""
+            choiceA : 
+            choiceB : 
+            choiceC : 
+            correct : 
+        },
+    // Question 7
+        {
+            question: ""
+            choiceA : 
+            choiceB : 
+            choiceC : 
+            correct : 
+        },
+    // Question 8
+        {
+            question: ""
+            choiceA : 
+            choiceB : 
+            choiceC : 
+            correct : 
+        },
+    // Question 9
+        {
+            question: ""
+            choiceA : 
+            choiceB : 
+            choiceC : 
+            correct : 
+        },
+    
+    ]
---
+++ script.js
@@ -0,0 +1,24 @@
+const start = document.getElementById("start");
+const quiz = document.getElementById("quiz");
+const question = document.getElementById("question");
+const counter = document.getElementById("counter");
+const timeGauge = document.getElementById("timeGauge");
+
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
+
+const progress = document.getElementById("progress");
+const scoreBoard = document.getElementById("scoreBoard");
+
+let lastQuestionIndex = questions.length - 1;
+let runningQuestionIndex = 0;
+ 
+function renderQuestion(){
+    let q = questions[runningQuestionIndex];
+    question.innerHTML = "<p>" + q.question + "<p>";
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+}
+
